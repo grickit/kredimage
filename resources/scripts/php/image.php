4f6696c04adabe27f7a8ad134db14d92a47482fc
Derek Hoagland <thegrickit@gmail.com>
2011-09-25 17:30:09 -0400

    Something this complicated needs documentation.

--- resources/scripts/php/image.php
+++ resources/scripts/php/image.php
@@ -1,61 +1,60 @@
 <?php
   include("secrets.php");
 
-  function loadImage($location) {
+  function loadImage($location) { // Load image from file
     $contents = file_get_contents($location);
     return imagecreatefromstring($contents);
   }
 
-  function getImageInfo($location) { return getimagesize($location); }
+  function getImageInfo($location) { return getimagesize($location); } // Get metadata about the image
 
-  function getImageType($location) {
+  function getImageType($location) { // Extract the filetype from the metadata
     $image_info = getImageInfo($location);
     return $image_info[2];
   }
 
-  function getImageMime($location) {
+  function getImageMime($location) { // Convert filetype to mimetype
     $image_info = getImageInfo($location);
     return image_type_to_mime_type($image_info[2]);
   }
 
-  function getImageWidth($image) { return imagesx($image); }
+  function getImageWidth($image) { return imagesx($image); } // Get the width of the image
 
-  function getImageHeight($image) { return imagesy($image); }
+  function getImageHeight($image) { return imagesy($image); } // Get the height of the image
 
-  function resizeImage($image,$image_type,$width,$height) {
-    $resized = imagecreatetruecolor($width,$height);
+  function resizeImage($image,$image_type,$width,$height) { // Resize the image
+    $resized = imagecreatetruecolor($width,$height); # Create a blank canvas
     $transparency = imagecolortransparent($image);
 
-    if($transparency >= 0) {
+    if($transparency >= 0) { # If the image already has a transparent color, inject it into the canvas
       $transparent_color = imagecolorsforindex($image,$transparency);
       $transparency = imagecolorallocate($resized,$transparent_color['red'],$transparent_color['green'],$transparent_color['blue']);
       imagefill($resized,0,0,$transparency);
       imagecolortransparent($resized,$transparency);
-
     }
-    elseif($image_type == IMAGETYPE_PNG) {
+    elseif($image_type == IMAGETYPE_PNG) { # All pngs need a base transparency
       $transparency_color = imagecolorallocatealpha($resized,0,0,0,127);
       imagefill($resized,0,0,$transparency_color);
     }
-    imagecopyresampled($resized,$image,0,0,0,0,$width,$height,getImageWidth($image),getImageHeight($image));
+    imagecopyresampled($resized,$image,0,0,0,0,$width,$height,getImageWidth($image),getImageHeight($image)); # Copy the image onto the canvas
     return $resized;
   }
 
-  function resizeImageToWidth($image,$image_type,$width) {
+  function resizeImageToWidth($image,$image_type,$width) { // Resize the image proportionally to a specific width
     $percent = $width / getImageWidth($image);
     $height = getImageHeight($image) * $percent;
     return resizeImage($image,$image_type,$width,$height);
   }
 
-  function resizeImageToHeight($image,$image_type,$height) {
+  function resizeImageToHeight($image,$image_type,$height) { // Resize the image proportionally to a specific height
     $percent = $height / getImageHeight($image);
     $width = getImageWidth($image) * $percent;
     return resizeImage($image,$image_type,$width,$height);
   }
 
-  function resizeImageToNumber($image,$image_type,$number) {
-    if ((getImageWidth($image) > $number) || (getImageHeight($image) > $number)) {
-      if (getImageWidth($image) >= getImageHeight($image)) {
+  function resizeImageToNumber($image,$image_type,$number) { // Resize the image proportionally so that the largest dimension equals a given number
+    if ((getImageWidth($image) > $number) || (getImageHeight($image) > $number)) { # This function is used blindly, and may not be needed
+      if (getImageWidth($image) >= getImageHeight($image)) { # Do we use width or height?
 	return resizeImageToWidth($image,$image_type,$number);
       }
       else {
@@ -67,13 +66,13 @@
     }
   }
 
-  function outputImage($image,$image_type) {
-    if( $image_type == IMAGETYPE_JPEG ) { imagejpeg($image); }
-    elseif( $image_type == IMAGETYPE_GIF ) { imagegif($image); }
-    elseif( $image_type == IMAGETYPE_PNG ) { imagepng($image); }
+  function outputImage($image,$image_type) { // Put the image to the browser
+    if($image_type == IMAGETYPE_JPEG) { imagejpeg($image); }
+    elseif($image_type == IMAGETYPE_GIF) { imagegif($image); }
+    elseif($image_type == IMAGETYPE_PNG) { imagepng($image); }
   }
 
-  function loadAndOutputImage($location) {
+  function loadAndOutputImage($location) { // Load an image and output it
     header("Content-type: ".getImageMime($location));
     $image = loadImage($location);
     $image_type = getImageType($location);
@@ -83,7 +82,7 @@
     exit();
   }
 
-  function loadResizeAndOutputImage($location,$number) {
+  function loadResizeAndOutputImage($location,$number) { // Load an image, resize it, and output it
     header("Content-type: ".getImageMime($location));
     $image = loadImage($location);
     $image_type = getImageType($location);
@@ -96,25 +95,25 @@
 
   if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    if (isset($_GET['json'])) {
+    if (isset($_GET['json'])) { # Want json?
       // TODO: Echo information about the image in JSON format
     }
     else {
-      if (isset($_GET['small'])) {
-	if (file_exists($small_directory.$id)) {
+      if (isset($_GET['small'])) { # Want small?
+	if (file_exists($small_directory.$id)) { # Already have small?
 	  loadAndOutputImage($small_directory.$id);
 	}
 	else {
-	  loadResizeAndOutputImage($full_directory.$id,600);
+	  loadResizeAndOutputImage($full_directory.$id,600); # Make small
 	}
       }
 
-      elseif (isset($_GET['thumb'])) {
-	if (file_exists($thumb_directory.$id)) {
+      elseif (isset($_GET['thumb'])) { # Want thumbnail?
+	if (file_exists($thumb_directory.$id)) { # Already have thumbnail?
 	  loadAndOutputImage($thumb_directory.$id);
 	}
 	else {
-	  loadResizeAndOutputImage($full_directory.$id,90);
+	  loadResizeAndOutputImage($full_directory.$id,90); # Make thumbnail
 	}
       }
 
