f50c273e30f7abd37aef23430f4d8cf7dafea9ee
Derek Hoagland <thegrickit@gmail.com>
2011-09-28 19:10:55 -0400

    Actually display the title and description and owner of the image.

--- art.php
+++ art.php
@@ -3,10 +3,23 @@
 
 ?>
 <?php // Processing the page
-
-  $image_id;
-  if(isset($_GET['id'])) {
+  if(isset($_GET['id']) && is_numeric($_GET['id']) && file_exists($full_directory.$_GET['id'])) {
+    $db_server = connectToDatabase();
     $image_id = $_GET['id'];
+    $image_id_db = mysql_real_escape_string($image_id);
+
+    $query = "SELECT user_registration.username FROM user_registration, image_upload WHERE image_upload.id = '$image_id_db' AND user_registration.id = image_upload.owner";
+    $result = mysql_query($query);
+    if(!$result) die("Couldn't lookup username: ".mysql_error());
+    $row = mysql_fetch_array($result);
+    $owner_username = $row['username'];
+
+    $query = "SELECT name, description FROM image_stats WHERE id = '$image_id_db'";
+    $result = mysql_query($query);
+    if(!$result) die("Couldn't lookup username: ".mysql_error());
+    $row = mysql_fetch_array($result);
+    $image_title = $row['name'];
+    $image_desc = $row['description'];
   }
   else {
     sendTo("upload.php");
@@ -22,8 +35,8 @@
   <a href="images/full/<?php echo $image_id; ?>.png"><img src="images/small/<?php echo $image_id; ?>.png"></a>
 </div>
 <div id="stats_page">
-  <span style="font-size: 24px;">Image Title</span><br>
-  by <a href="profile.php?a=foo">Foo</a><br>
-  <p>Lorem ipsum foo bar baz</p>
+  <span style="font-size: 24px;"><?php echo $image_title; ?></span><br>
+  by <a href="profile.php?a=<?php echo $owner_username; ?>"><?php echo $owner_username; ?></a><br>
+  <p><?php echo $image_desc; ?></p>
 </div>
 <?php include("resources/footer.html"); ?>
