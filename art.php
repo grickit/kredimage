f49f587966ac4f7e3f1464cc73b198b12b732e06
Derek Hoagland <thegrickit@embarqmail.com>
2011-09-28 13:41:52 -0400

    A page for viewing art. Work in progress.

---
+++ art.php
@@ -0,0 +1,25 @@
+<?php include("resources/scripts/php/kredimage.php"); ?>
+<?php // Functions
+
+?>
+<?php // Processing the page
+  $image_id;
+  if(isset($_GET['id'])) {
+    $image_id = $_GET['id'];
+  }
+  else {
+    sendTo("upload.php");
+  }
+?>
+<?php
+  include("resources/scripts/php/login.php");
+  include("resources/header.php");
+?>
+<style type="text/css">@import url("resources/styles/art.css");</style>
+<div id="art_container">
+  <a href="images/full/<?php echo $image_id; ?>.png"><img src="images/small/<?php echo $image_id; ?>.png"></a>
+</div>
+<div id="stats_page">
+  <span style="font-size: 24px;">Image Title</span>
+</div>
+<?php include("resources/footer.html"); ?>
