c40eb3b481f8e52966f150da05e47d3c9b4c0efc
Derek Hoagland <thegrickit@gmail.com>
2011-10-12 13:04:42 -0400

    Beginnings of comment submittal.

--- art.php
+++ art.php
@@ -1,19 +1,23 @@
-<?php include("resources/scripts/php/kredimage.php"); ?>
+<?php
+  include("resources/scripts/php/kredimage.php"); 
+  include("resources/scripts/php/login.php");
+?>
 <?php // Functions
 
 ?>
 <?php // Processing the page
+  $db_server = connectToDatabase();
+
   if(isset($_GET['id']) && is_numeric($_GET['id']) && file_exists($full_directory.$_GET['id'])) {
-    $db_server = connectToDatabase();
     $image_id = $_GET['id'];
     $image_id_db = mysql_real_escape_string($image_id);
 
-    $query = "SELECT user_registration.username FROM user_registration, image_upload WHERE image_upload.id = '$image_id_db' AND user_registration.id = image_upload.owner";
+    $query = "SELECT user_registration.username, user_registration.id FROM user_registration, image_upload WHERE image_upload.id = '$image_id_db' AND user_registration.id = image_upload.owner";
     $result = mysql_query($query);
     if(!$result) die("Couldn't lookup username: ".mysql_error());
     $row = mysql_fetch_array($result);
     $owner_username = $row['username'];
-
+    $owner_id = $row['id'];
     $query = "SELECT name, description FROM image_stats WHERE id = '$image_id_db'";
     $result = mysql_query($query);
     if(!$result) die("Couldn't lookup username: ".mysql_error());
@@ -24,10 +28,17 @@
   else {
     sendTo("upload.php");
   }
+
+  if(isset($_GET['comment']) && isset($_POST['c_text'])) {
+    if($logged_in == true) {
+      $text = $_POST['c_text'];
+      $query = "INSERT INTO image_comments VALUES (NULL,'$image_id','$owner_id','$text')";
+      $result = mysql_query($query);
+    }
+  }
 ?>
 <?php
   $mini_login = true;
-  include("resources/scripts/php/login.php");
   include("resources/header.php");
 ?>
 <style type="text/css">@import url("resources/styles/art.css");</style>
@@ -47,7 +58,7 @@
   </div>
 
   <div id="comment_entry_area">
-    <form id="content_entry_form">
+    <form id="content_entry_form" method="POST" action="art.php?id=<?php echo $image_id ?>&comment">
       <textarea name="c_text"></textarea>
       <input type="submit" name="c_submit" value="Submit">
     </form>
