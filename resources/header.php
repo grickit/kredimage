58e2fc95ca9d1625a2a85218b1f7005955ba771b
derek <derek@ariadne-ubuntu.(none)>
2011-10-26 14:06:08 -0400

    Expand logo placeholder horizontally too.

--- resources/header.php
+++ resources/header.php
@@ -10,7 +10,7 @@
 
       <div id="head_container">
 	<div id="head_content">
-	  <img id="kredimage_logo" src="" width="145" height="60" alt="Kredimage" style="margin-top: 5px; margin-left: 5px; float: left;">
+	  <img id="kredimage_logo" src="" width="180" height="60" alt="Kredimage" style="margin-top: 5px; margin-left: 5px; float: left;">
 	  <form id="search_form" method="get" action="">
 	  </form>
 	  <?php
