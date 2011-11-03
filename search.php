0af54b749132ab3dd1f1b626fbd7a25807e9e77c
Derek Hoagland <thegrickit@gmail.com>
2011-11-02 22:09:48 -0400

    The beginnings of the image search page.

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
-  include("resources/scripts/php/kredimage.php"); 
+  include("resources/scripts/php/kredimage.php");
   include("resources/scripts/php/login.php");
 ?>
 <?php // Functions
@@ -8,11 +8,23 @@
 <?php // Processing the page
   $db_server = connectToDatabase();
 
+  $mode = "all";
+  if(isset($_GET['mode'])) $mode = $_GET['mode'];
 ?>
 <?php
   $mini_login = true;
   include("resources/header.php");
 ?>
 <style type="text/css">@import url("resources/styles/art.css");</style>
-
+<?php
+  switch($mode) {
+    case "all":
+      $query = "SELECT * FROM image_stats ORDER BY id DESC";
+      $result = mysql_query($query);
+      while ($row = mysql_fetch_assoc($result)) {
+	echo '<img src="images/thumb/'.$row['id'].'.png">';
+      }
+      break;
+  }
+?>
 <?php include("resources/footer.html"); ?>
