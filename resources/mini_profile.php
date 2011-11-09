e6d1d99424865e541a560f6668339d62316ff3c3
Derek Hoagland <thegrickit@gmail.com>
2011-11-09 13:10:39 -0500

    Add a link to the upload page on the mini profile.

--- resources/mini_profile.php
+++ resources/mini_profile.php
@@ -1,4 +1,5 @@
 <div id="login_container">
   <a href="search.php?mode=user&user=<?php echo $_SESSION['username']; ?>"><?php echo $_SESSION['username']; ?></a><br>
+  <a href="upload.php">Upload</a><br>
   <a href="login.php?logout&referer=<?php echo urlencode($_SERVER['REQUEST_URI']); ?>">Logout</a>
 </div>
